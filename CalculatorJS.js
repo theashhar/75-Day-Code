b91557e22a56653e1740b9a3e491d75b3a1dd8d2
Ashhar Ali Ahmed <ashhar1111981@gmail.com>
2023-08-05 00:31:57 +0530

    day 19/75 || Tip Calculator Javasctipt

---
+++ CalculatorJS.js
@@ -0,0 +1,37 @@
+//Functions
+const calculateBill = () => {
+    
+}
+const billInput = () => {
+    console.log(noOfPeople)
+}
+const tipInput = () => {
+    
+}
+const noOfPeopleDiv = () => {
+    
+}
+const perPersonTotal = () => {
+    
+}
+
+
+
+// const billInput = () => {
+//     console.log(noOfPeople)
+// }
+// const noOfPeopleDivE = () => {
+    
+// }
+// const perPersonTotal = () => {
+    
+// }
+
+const billInputElement = document.getElementById('billInput')
+const tipInputElement = document.getElementById('tipInput')
+const noOfPeopleDivElement = document.getElementById('noOfPeopleDiv')
+const perPersonTotalElement = document.getElementById('perPersonTotal')
+
+
+let noOfPeople = Number(noOfPeopleDiv.innerText)
+// console.log(noOfPeople )
