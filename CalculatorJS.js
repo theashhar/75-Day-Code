765689fb75e5df021972c4f7385561db8da79137
Ashhar Ali Ahmed <ashhar1111981@gmail.com>
2023-08-06 01:15:57 +0530

    day 20/75 || Building Tip and Bill calculator App

--- CalculatorJS.js
+++ CalculatorJS.js
@@ -1,37 +1,46 @@
-//Functions
-const calculateBill = () => {
-    
-}
-const billInput = () => {
-    console.log(noOfPeople)
-}
-const tipInput = () => {
-    
-}
-const noOfPeopleDiv = () => {
-    
-}
-const perPersonTotal = () => {
-    
-}
-
-
-
-// const billInput = () => {
-//     console.log(noOfPeople)
-// }
-// const noOfPeopleDivE = () => {
-    
-// }
-// const perPersonTotal = () => {
-    
-// }
-
 const billInputElement = document.getElementById('billInput')
 const tipInputElement = document.getElementById('tipInput')
 const noOfPeopleDivElement = document.getElementById('noOfPeopleDiv')
 const perPersonTotalElement = document.getElementById('perPersonTotal')
+const minusBtnElement = document.getElementById('minusBtn')
+const plusBtnElement = document.getElementById('plusBtn')
+const calculateBtnElement = document.getElementById('calculateBtn')
 
+//Functions
 
-let noOfPeople = Number(noOfPeopleDiv.innerText)
-// console.log(noOfPeople )
+const billInput = () => {
+    // console.log(noOfPeople)
+    // console.log(billInputElement.value)
+
+}
+const tipInput = () => {
+    // console.log(noOfPeople)
+
+}
+// const noOfPeopleDiv = () => {
+//     return null
+// }
+const perPersonTotal = () => {
+    
+}
+decreasePeople = () => {
+    minusBtnElement.onclick = () => {
+        if (noOfPeopleDivElement.innerText > 1)
+        noOfPeopleDivElement.innerText--
+    }
+}
+increasePeople = () => {
+    plusBtnElement.onclick = () => {
+        noOfPeopleDivElement.innerText++
+    }
+}
+const calculateBill = () => {
+    calculateBtnElement.onclick = () {
+    const finalOutput = Number((tipInputElement.value/100)*billInputElement.value)/noOfPeopleDivElement.innerText
+    }
+    return finalOutput
+
+}
+
+// let noOfPeople = Number(noOfPeopleDiv.innerText)
+// console.log(calculateBill)
