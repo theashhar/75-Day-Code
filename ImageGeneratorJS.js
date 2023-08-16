b16eb50d0f413011154285ee795c6551c84d3fb3
Ashhar Ali Ahmed <ashhar1111981@gmail.com>
2023-08-16 23:23:14 +0530

    day 31/75

--- ImageGeneratorJS.js
+++ ImageGeneratorJS.js
@@ -3,6 +3,7 @@ const imageDogElement = document.getElementById('imageDog')
 const generateButtonElement = document.getElementById('generateButton')
 const searchHeroElement = document.getElementById('searchHero')
 const searchBtnElement = document.getElementById('searchBtn')
+const heroNameElement = document.getElementById('heroName')
 
 const SuperheroToken = 1389261215019547
 const BASE_URL = `https://superheroapi.com/api.php/1389261215019547`
@@ -10,7 +11,8 @@ const generateHero = (id) => {
     fetch(`${BASE_URL}/${id}`)
     .then (response => response.json())
     .then(json => {
-        console.log(json)
+        console.log(json.powerstats.intelligence)
+        heroNameElement.innerText = (json.name) 
         imageDogElement.innerHTML = `<img src='${json.image.url}' height='500px' />`
     })
 }
@@ -21,7 +23,7 @@ const searchHero = () => {
     fetch(`${BASE_URL}/search/${searchHeroElement.value}`)
     .then (response => response.json())
     .then(json => {
-        console.log(json.results[0])
+        heroNameElement.innerText = (json.results[0].name)
         imageDogElement.innerHTML = `<img src='${json.results[0].image.url}' height='500px' />`
     })
 }
