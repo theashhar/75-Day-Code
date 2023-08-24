51e28a718a83e604154d8b5fe9a7640e9db7cfad
Ashhar Ali Ahmed <ashhar1111981@gmail.com>
2023-08-25 00:25:27 +0530

    day 39/75 || Weather App degree fetched and updated in the DOM

--- WeatherWebapp.js
+++ WeatherWebapp.js
@@ -1,21 +1,66 @@
-const url2 = 'https://open-weather13.p.rapidapi.com/city/landon';
-const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': '858689371bmshf8643ba1691968dp16c6afjsn01257654bc2a',
-		'X-RapidAPI-Host': 'open-weather13.p.rapidapi.com'
-	}
-};
-// const weather1 = async () => {
-// try {
-// 	const response = await fetch(url2, options);
-// 	const result = await response.text();
-// 	console.log(result);
-// } catch (error) {
-// 	console.error(error);
-// }
+const degreeElement = document.getElementById('degree')
+
+const apiKey = '52436686893145c585a121019232408';
+// `https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${location}`
+const url = `https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=sikkim`
+const weather1 = async () => {
+try {
+	const response = await fetch(url);
+	const result = await response.json();
+	return result
+	
+} catch (error) {
+	console.error(error);
+}
+}
+
+(async () => {
+	degree = await weather1()
+	console.log(degree.current.temp_c)
+	degreeElement.innerText = `${Math.ceil(degree.current.temp_c)}°`
+})()
+
+// getWeatherData()
+// searchCity()
+// showWeatherData() ->updates on the DOM
+
+
+
+
+
+
+
+
+
+
+// const apiKey = "52436686893145c585a121019232408";
+// // const location = "New+York"; // Replace with your desired location
+
+// async function fetchData() {
+//   const apiUrl = `https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=New+York`;
+
+//   try {
+//     const response = await fetch(apiUrl);
+//     if (!response.ok) {
+//       throw new Error(`API request failed with status: ${response.status}`);
+//     }
+
+//     const data = await response.json();
+//     return data;
+//   } catch (error) {
+//     console.error("Error fetching data:", error.message);
+//     return null;
+//   }
 // }
 
+// // Call the async function to fetch data
 // (async () => {
-// 	await weather1()
-// })()
+//   const weatherData = await fetchData();
+
+//   if (weatherData) {
+//     // Process the data received from the API
+//     console.log(weatherData);
+//   } else {
+//     console.log("Failed to fetch weather data.");
+//   }
+// })();
