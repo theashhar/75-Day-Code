439207b7b40ce339ac5ea24434d4691d4c8e94e8
Ashhar Ali Ahmed <ashhar1111981@gmail.com>
2023-08-26 00:44:10 +0530

    day 40/75 || Weather APP main functionality completed, search feature added, weather details reflects in DOM

--- WeatherWebapp.js
+++ WeatherWebapp.js
@@ -1,27 +1,72 @@
 const degreeElement = document.getElementById('degree')
+const conditionTextElement = document.getElementById('conditionText')
+const weatherIconElement = document.getElementById('weatherIcon')
+const searchCityElement = document.getElementById('searchCity')
+const weatherLocationElement = document.getElementById('weatherLocation')
+const regionElement = document.getElementById('region')
+const dateElement = document.getElementById('date')
 
+
+
+const location1 = "bangalore"
 const apiKey = '52436686893145c585a121019232408';
-// `https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${location}`
-const url = `https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=sikkim`
-const weather1 = async () => {
-try {
-	const response = await fetch(url);
-	const result = await response.json();
-	return result
+const base_Url = `https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=`
+const url = `https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${location1}`
+// const weather1 = async () => {
+// try {
+// 	const response = await fetch(url);
+// 	const result = await response.json();
+// 	console.log(result)
+// 	return result
+
+// } catch (error) {
+// 	console.error(error);
+// 	let errorMark = `?`
+// 	console.error(errorMark);
+// 	return errorMark
+// 	}
+// }
+
+
+const searchCity = async () => {
+	try {
+		const response = await fetch(`${base_Url}${searchCityElement.value}`);
+		console.log(response)
+		const result = await response.json();
+		return result
 	
-} catch (error) {
-	console.error(error);
-}
-}
+	} catch (error) {
+		console.error(error);
+		let errorMark = `?`
+		console.error(errorMark);
+		return errorMark
+		}}
 
-(async () => {
-	degree = await weather1()
-	console.log(degree.current.temp_c)
+const getWeatherData = async () => {
+	degree = await searchCity()
+	console.log(degree)
+
 	degreeElement.innerText = `${Math.ceil(degree.current.temp_c)}°`
-})()
 
+	conditionTextElement.innerText = degree.current.condition.text
+	
+	if (degree.location.name == degree.location.region)
+		region = degree.location.country
+	else
+		region  = degree.location.region
+	weatherLocationElement.innerText = (degree.location.name)
+	regionElement.innerText = region
+
+	dateElement.innerText = degree.location.localtime
+
+	console.log(degree.current.condition.icon)
+	// `<img src='${json.image.url}'  />`
+	weatherIconElement.src = `${degree.current.condition.icon}`
+}
 // getWeatherData()
-// searchCity()
+
+
+
 // showWeatherData() ->updates on the DOM
 
 
