925c4c1ffdb2025e6d9e20755d27ead82c43d78a
Ashhar Ali Ahmed <ashhar1111981@gmail.com>
2023-09-04 23:48:26 +0530

    day 50/75 || TTT Game xplaceMark

--- Tic Tac Toe/TTTscript.js
+++ Tic Tac Toe/TTTscript.js
@@ -1,25 +1,54 @@
 const cellElements = document.querySelectorAll('[cellData]')
+const boardElement = document.getElementById('board')
 const X_CLASS = 'x';
 const CIRCLE_CLASS = 'circle';
+WINNING_COMBINATIONS = [
+    [0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [2, 4, 6], [0, 4, 8],
+]
 let circleTurn
 
 cellElements.forEach(cell => {
-    cell.addEventListener('click', boardClick, { once : true })
+    cell.addEventListener('click', cellClick, { once : true })
 })
+setBoardHoverClass()
 
-function boardClick(e) {
-    const cell = e.target
-    const currentClass = circleTurn ? X_CLASS : CIRCLE_CLASS
-placeMark(cell, currentClass)
-
+function cellClick(e) {
 // place Mark
+    const cell = e.target
+    const currentClass = circleTurn ? CIRCLE_CLASS : X_CLASS 
+    placeMark(cell, currentClass) //adds currentClass(x/circle) to e.target
 // Check for win
+    if (checkWin(currentClass))
+        console.log(`${currentClass} winner`)
 // check for Draw 
 // switch turns
-console.log('sd')
-
+    switchTurns() 
+    setBoardHoverClass()   
 }    
 
 function placeMark(cell, currentClass) {
- cell.classList.add(currentClass)
+    cell.classList.add(currentClass)
 }
+function switchTurns() {
+    circleTurn = !circleTurn // sets to opposite
+}
+function setBoardHoverClass() {
+    boardElement.classList.remove(X_CLASS)
+    boardElement.classList.remove(CIRCLE_CLASS)
+    if (circleTurn){
+     boardElement.classList.add(CIRCLE_CLASS)
+    } else {
+     boardElement.classList.add(X_CLASS)
+   }
+//    console.log(boardElement.classList)
+}
+
+function checkWin (currentClass) {
+    const win = WINNING_COMBINATIONS.some(combination => {
+        return combination.every(index => {
+            return cellElements[index].classList.contains(currentClass)
+        })
+    })
+    return win
+}
+// console.log(checkWin('x'))
