afd4907b9b571b8cbb3e9278505f64a2e0576098
Ashhar Ali Ahmed <ashhar1111981@gmail.com>
2023-09-15 16:55:09 +0530

    check

--- theAshhar.com/ashharScript.js
+++ theAshhar.com/ashharScript.js
@@ -1,31 +1,33 @@
-const slider = document.querySelector('.slider');
-const circles = document.querySelectorAll('.circle');
 
-let currentIndex = 0;
 
-circles.forEach((circle, index) => {
-    circle.addEventListener('click', () => {
-        moveToSlide(index);
-    });
-});
+// const slider = document.querySelector('.slider');
+// const circles = document.querySelectorAll('.circle');
 
-function moveToSlide(index) {
-    currentIndex = index;
-    updateSliderPosition();
-    updateActiveCircle();
-}
+// let currentIndex = 0;
 
-function updateSliderPosition() {
-    const translateX = -currentIndex * 100;
-    slider.style.transform = `translateX(${translateX}%)`;
-}
+// circles.forEach((circle, index) => {
+//     circle.addEventListener('click', () => {
+//         moveToSlide(index);
+//     });
+// });
 
-function updateActiveCircle() {
-    circles.forEach((circle, index) => {
-        if (index === currentIndex) {
-            circle.classList.add('active');
-        } else {
-            circle.classList.remove('active');
-        }
-    });
-}
+// function moveToSlide(index) {
+//     currentIndex = index;
+//     updateSliderPosition();
+//     updateActiveCircle();
+// }
+
+// function updateSliderPosition() {
+//     const translateX = -currentIndex * 100;
+//     slider.style.transform = `translateX(${translateX}%)`;
+// }
+
+// function updateActiveCircle() {
+//     circles.forEach((circle, index) => {
+//         if (index === currentIndex) {
+//             circle.classList.add('active');
+//         } else {
+//             circle.classList.remove('active');
+//         }
+//     });
+// }
