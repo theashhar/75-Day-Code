4909430980a5047d2b5f323d1ecc46258b5592c6
Ashhar Ali Ahmed <ashhar1111981@gmail.com>
2023-09-29 00:00:38 +0530

    day 73/75 || building snake game

--- NostalgicSnakeGame/snake.js
+++ NostalgicSnakeGame/snake.js
@@ -1,21 +1,115 @@
-var blockSize = 22;
-var rows = 22;
-var cols = 22;
-var playground;
-var context;
+const blockSize = 22;
+const rows = 30;
+const cols = 30;
+let playground;
+let context;
 
-console.log("to")
+//snake
+let snakeX = blockSize * 5
+let snakeY = blockSize * 5
+let velocityX = 0
+let velocityY = 0
+//food
+let foodX
+let foodY
 
 window.onload = function() {
-    playground = document.getElementById('playground')
-    playground.style.width = cols * blockSize + 'px';
-    playground.style.height = rows * blockSize + 'px';
-    context = playground.getContext('2d') // for drawing on board
- 
-    update();
+  startGame()
 }
+function startGame() {
+  playground = document.getElementById("playground");
+  context = playground.getContext("2d"); // for drawing on board
+  playground.width = cols * blockSize;
+  playground.height = rows * blockSize;
+
+  document.addEventListener("keydown", changeDirection)
+  placeFood();
+  setInterval(update, 100)
+};
 
 const update = () => {
-    context.fillStyle = 'black'
-    context.fillRect(0, 0, playground.width, playground.height)
+  context.fillStyle = "rgb(45, 58, 73)";
+  context.fillRect(0, 0, playground.width, playground.height);
+
+  context.fillStyle = "red";
+  context.fillRect(foodX, foodY, blockSize, blockSize);
+
+  context.fillStyle = "lime";
+  snakeX += velocityX * blockSize
+  snakeY += velocityY * blockSize
+  context.fillRect(snakeX, snakeY, blockSize, blockSize);
+  
+  if (eatFood(snakeX,snakeY)){
+    updateScore()
+    increaseSnakeSize()
+  }
 }
+
+const placeFood = () => {
+    foodX = Math.floor(Math.random() * cols) * blockSize
+    foodY = Math.floor(Math.random() * rows) * blockSize
+}
+const eatFood = (x,y) => {
+  if (foodX===x && foodY===y)
+  {console.log("eaten")
+  placeFood();
+  return true
+}
+}
+let score = 0
+const updateScore = () => {
+    scoreElement = document.getElementById('score')
+    score++
+    scoreElement.innerText = `score: ${score}`  
+    console.log(scoreElement.innerText)
+}
+const increaseSnakeSize = () => {
+  context.fillRect(snakeX, snakeY, blockSize*2, blockSize);
+}
+
+const changeDirection = (e) => {
+  if (e.key == "ArrowUp" && velocityY!=1) {
+  velocityX = 0
+  velocityY = -1
+  }
+  else if (e.key == "ArrowRight" && velocityX!=-1) {
+    velocityX = 1
+    velocityY = 0
+  }
+  else if (e.key == "ArrowDown"&& velocityY!=-1) {
+    velocityX = 0
+    velocityY = 1
+  }
+  else if (e.key == "ArrowLeft" && velocityX!=1) {
+    velocityX = -1
+    velocityY = 0
+  }
+}
+
+
+// const update = () => {
+//   context.fillStyle = "rgb(45, 58, 73)";
+//   context.fillRect(0, 0, playground.width, playground.height);
+
+//   context.fillStyle = "red";
+//   context.fillRect(foodX, foodY, blockSize, blockSize);
+
+//   context.fillStyle = "lime";
+//   snakeX += velocityX * blockSize
+//   snakeY += velocityY * blockSize
+//   context.fillRect(snakeX, snakeY, blockSize, blockSize);
+
+//   eatFood(snakeX,snakeY)
+
+// }
+
+// const placeFood = () => {
+//     foodX = Math.floor(Math.random() * cols) * blockSize
+//     foodY = Math.floor(Math.random() * rows) * blockSize
+// }
+// const eatFood = (x,y) => {
+//   if (foodX===x && snakeY===y)
+//   console.log("eaten")
+//   placeFood()
+//   return true
+// }
